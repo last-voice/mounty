d65118e7e6889f56b77ad18997bbf2b84f057d74
stefan <stefan.freise@code-x.de>
2022-09-29 00:06:16 +0200

    git cleanup

--- howto-deploy.md
+++
@@ -1,5 +0,0 @@
-## ZIP for Curse
-
-```
-zip -r TLV_Mounty TLV_Mounty -x \*/.DS_Store
-```
--- mkzip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -f TLV_Mounty/TLV_Mounty.zip
-
-cd "/Applications/World of Warcraft/_retail_/Interface/Addons"
-
-zip -r TLV_Mounty/TLV_Mounty.zip TLV_Mounty/localization TLV_Mounty/Bindings.xml TLV_Mounty/CHANGELOG.md TLV_Mounty/LICENSE TLV_Mounty/README.md TLV_Mounty/TLV_Mounty.lua TLV_Mounty/TLV_Mounty.toc
